c82ecfdfe2816393694d70ddce3d2ba7be102075
pyason <paul.yason@gmail.com>
2018-05-13 00:25:27 -0400

    //com.pyason.HereMark_develop
    Removing GoogleApiClient and use PlacePicker for choosing current place.

--- app/src/main/java/com/pyason/heremark/MainActivity.java
+++ app/src/main/java/com/pyason/heremark/MainActivity.java
@@ -1,7 +1,7 @@
 package com.pyason.heremark;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -12,87 +12,86 @@ import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.ConnectivityManager;
-import android.net.Network;
 import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.os.ResultReceiver;
+import android.os.Looper;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-import android.app.Activity;
 
-import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.CommonStatusCodes;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
-import java.text.DateFormat;
-import java.util.Date;
-
-import me.drakeet.materialdialog.MaterialDialog;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class MainActivity extends AppCompatActivity {
 
-    private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation, mCurrentLocation;
+    protected GeoDataClient mGeoDataClient;
+    protected PlaceDetectionClient mPlaceDetectionClient;
+    protected FusedLocationProviderClient mFusedLocationProviderClient;
     protected LocationRequest mLocationRequest;
+    protected LocationCallback mLocationCallback;
     private MainList listFragment;
     private FragmentManager mFragmentManager;
-    //private boolean mRequestStatus = true;
-    private MaterialDialog dialog;
 
     private double currentLatitude, currentLongitude;
+    protected Location currentLocation, selectedLocation;
     protected String lastUpdateTime, addressMessage = null;
     protected CharSequence[] addressMessageList = null;
-    private AddressResultReceiver resultReceiver;
-    public boolean mAddressRequested = false;
+    protected float currentLikelihoodValue, newLikelihoodValue;
+    protected Place currentPlace, selectedPlace;
+    protected double maxDistance = 1000;
 
     private final String FRAGMENT_TAG = "main_list_tag";
-    private final String SELECT_ADDR_TAG = "addr_select_tag";
+    private final String G_PLACE_SERVICE_ERROR = "g_places_service_error";
+    private final String CURRENT_PLACE_ERROR = "current_place_error";
     private static final int REQUEST_LOCATION = 0;
     protected static final int REQUEST_CHECK_SETTINGS = 1;
+    protected final int PLACE_PICKER_REQUEST = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addOnConnectionFailedListener(this)
-                .addConnectionCallbacks(this)
-                .build();
+        mGeoDataClient = Places.getGeoDataClient(this);
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this);
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(60000);
+        mLocationRequest.setFastestInterval(10000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationCallback = new LocationCallback();
 
         mFragmentManager = getFragmentManager();
 
@@ -112,42 +111,76 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         FloatingActionButton floatingActionButton = findViewById(R.id.fab_button);
 
-        //LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
-
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkLocationSettings(MainActivity.this);
-                
-                if (checkAirPlaneMode(getApplicationContext())) {
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i("Permissions", "Location Permission not granted, request for permission.");
+                    // Should we show an explanation?
+                    PermissionUtil.requestForPermissions(MainActivity.this, listFragment, REQUEST_LOCATION);
+
+                }
+                else if (checkAirPlaneMode(getApplicationContext())) {
                     Snackbar.make(findViewById(R.id.coordinator_layout), "Air Plane Mode is ON, Please turn off to get location", Snackbar.LENGTH_LONG).show();
                 }
                 else if (!checkNetwork()) {
                     Snackbar.make(findViewById(R.id.coordinator_layout), "Unable to reach network, please check network settings", Snackbar.LENGTH_LONG).show();
                 }
                 else {
-                    mAddressRequested = true;
+                    PlacePicker.IntentBuilder placeBuilder = new PlacePicker.IntentBuilder();
+                    mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+                    mLocationCallback = new LocationCallback() {
+                        @Override
+                        public void onLocationResult(LocationResult locationResult) {
+                            List<Location> locationList = locationResult.getLocations();
+                            if (locationList.size()>0) {
+                                //Last location is the latest
+                                currentLocation = locationList.get(locationList.size() - 1);
+                            }
+                        }
+                    };
 
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle(R.string.select_location)
-                            .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    //AddressChooserDialogFragment.this.getDialog().dismiss();
+                    try{
+                        startActivityForResult(placeBuilder.build(MainActivity.this), PLACE_PICKER_REQUEST);
+                    }
+                    catch (GooglePlayServicesNotAvailableException e) {
+                        Log.e(G_PLACE_SERVICE_ERROR, "Google places service not availabe...");
+                        Snackbar.make(findViewById(R.id.coordinator_layout), "Google Play Service is currently unavailable, please check your connections", Snackbar.LENGTH_LONG).show();
+                    }
+                    catch (GooglePlayServicesRepairableException er) {
+                        Log.e(G_PLACE_SERVICE_ERROR, "Google places service may not be installed or up to date...");
+                        Snackbar.make(findViewById(R.id.coordinator_layout), "Please make sure you have installed and make sure Google Play service is up to date", Snackbar.LENGTH_LONG).show();
+                        //TODO: show installation dialog to user
+                    }
+
+                    /*Task<PlaceLikelihoodBufferResponse> currentPlaceResult = mPlaceDetectionClient.getCurrentPlace(null);
+                    currentLikelihoodValue = 0;
+                    currentPlaceResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+                        @Override
+                        public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                                PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+                                for (PlaceLikelihood placeLikelihood: likelyPlaces) {
+                                    newLikelihoodValue = placeLikelihood.getLikelihood();
+                                    if (newLikelihoodValue > currentLikelihoodValue) {
+                                        currentLikelihoodValue = newLikelihoodValue;
+                                        currentPlace = placeLikelihood.getPlace();
+                                    }
                                 }
-                            })
-                            .setItems(addressMessageList, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    listFragment.list.add((String)addressMessageList[i]);
-                                    listFragment.mAdapter.notifyDataSetChanged();
-                                }
-                            });
-                    builder.create().show();
+                                likelyPlaces.release();
+                            } else {
+                                Log.e(CURRENT_PLACE_ERROR, "getCurrentPlace is not working...");
+                            }
+                        }
+                    });*/
                 }
             }
         });
-
     }
 
     @Override
@@ -175,118 +208,32 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onStart() {
         super.onStart();
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(LocationServices.API)
-                    .addOnConnectionFailedListener(this)
-                    .addConnectionCallbacks(this)
-                    .build();
-        }
-        mGoogleApiClient.connect();
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
 
+        if (mFusedLocationProviderClient != null) {
+            mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if(mGoogleApiClient.isConnected()) {
-            LocationRequest locationRequest = createLocationRequest();
-            startLocationUpdates(locationRequest);
-        }
-    }
 
-    @Override
-    public void onConnected(Bundle bundle) {
-/*        if (ContextCompat.checkSelfPermission(this,
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
             Log.i("Permissions", "Location Permission not granted, request for permission.");
             // Should we show an explanation?
-            PermissionUtil.requestForPermissions(this, listFragment, REQUEST_LOCATION);
+            PermissionUtil.requestForPermissions(MainActivity.this, listFragment, REQUEST_LOCATION);
 
-        }else {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         }
-
-        if (mLastLocation != null) {
-            Log.i("Main", "Last location exists");
-        }*/
-
-        LocationRequest locationRequest = createLocationRequest();
-        startLocationUpdates(locationRequest);
-
-        if (mAddressRequested) {
-            startAddressLookUp();
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-        // Making sure GoogleAPIClient is connected
-
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(LocationServices.API)
-                    .addOnConnectionFailedListener(this)
-                    .addConnectionCallbacks(this)
-                    .build();
-        }
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        mCurrentLocation = location;
-        currentLatitude = mCurrentLocation.getLatitude();
-        currentLongitude = mCurrentLocation.getLongitude();
-        lastUpdateTime = DateFormat.getDateTimeInstance().format(new Date());
-    }
-
-
-    protected LocationRequest createLocationRequest() {
-
-        mLocationRequest = LocationRequest.create();
-
-        mLocationRequest.setInterval(8000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        return mLocationRequest;
-    }
-
-    protected void startLocationUpdates(LocationRequest request) {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            Log.i("Permissions", "Location Permission not granted, request for permission.");
-            // Should we show an explanation?
-            PermissionUtil.requestForPermissions(this, listFragment, REQUEST_LOCATION);
-
-        } else{
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, request, this);
-        }
-    }
-
-    protected void startAddressLookUp() {
-        Intent intent = new Intent(this,AddressLookUpService.class);
-        resultReceiver = new AddressResultReceiver(new Handler());
-        intent.putExtra(AddressLookUpService.Constants.RECEIVER, resultReceiver);
-        intent.putExtra(AddressLookUpService.Constants.LOCATION_DATA_EXTRA, mCurrentLocation);
-        startService(intent);
+        mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
     }
 
     protected void showLocationSettings() {
@@ -337,7 +284,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         locationSettingsResponseTask.addOnSuccessListener(activity, new OnSuccessListener<LocationSettingsResponse>() {
             @Override
             public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                startAddressLookUp();
+
             }
         });
 
@@ -363,32 +310,28 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 }
             }
         });
-
-/*        int locationMode = 0;
-        String locationProviders;
-
-        try {
-            locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
-        } catch (Settings.SettingNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return locationMode != Settings.Secure.LOCATION_MODE_OFF && locationMode != Settings.Secure.LOCATION_MODE_SENSORS_ONLY;*/
     }
 
-    @SuppressLint("ParcelCreator")
-    public class AddressResultReceiver extends ResultReceiver {
-        AddressResultReceiver(Handler handler) {
-            super(handler);
-        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                 selectedPlace = PlacePicker.getPlace(this, data);
 
-        //Parcelable.Creator CREATOR = ResultReceiver.CREATOR;
+                if (currentLocation != null && selectedPlace != null) {
 
-        @Override
-        protected void onReceiveResult(int resultCode, Bundle result) {
-            if (resultCode == AddressLookUpService.Constants.SUCCESS) {
-                //addressMessage = result.getString(AddressLookUpService.Constants.RESULT_DATA_KEY);
-                addressMessageList = result.getCharSequenceArray(AddressLookUpService.Constants.RESULT_DATA_KEY);
+                    selectedLocation = new Location("selectedLocation");
+                    selectedLocation.setLongitude(selectedPlace.getLatLng().longitude);
+                    selectedLocation.setLatitude(selectedPlace.getLatLng().latitude);
+
+                    if (selectedLocation.distanceTo(currentLocation) > maxDistance) {
+                        Snackbar.make(findViewById(R.id.coordinator_layout), "Hey, do select nearby places, don't make it up!", Snackbar.LENGTH_LONG).show();
+                    } else {
+                        listFragment.list.add(selectedPlace.getAddress().toString());
+                        listFragment.mAdapter.notifyDataSetChanged();
+                    }
+                }
             }
         }
     }
